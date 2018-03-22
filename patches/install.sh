19a6054b8d7e5fb74f2fb10aedb215b8f4e92340
bju2000 <bju2000@gmail.com>
2018-03-22 19:22:17 +0900

    Update install.sh
    soong_config.mk does not exist

--- patches/install.sh
+++ patches/install.sh
@@ -4,7 +4,7 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="bionic build/make/core build/soong frameworks/av frameworks/base hardware/interfaces system/core vendor/lineage hardware/ril"
+dirs="bionic build/make/core build/soong frameworks/av frameworks/base hardware/interfaces system/core hardware/ril"
 
 # red + nocolor
 RED='\033[0;31m'
